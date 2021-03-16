636f2d217d736d492a5d67c6054fd2d9264a38f9
radyvoniukkate <radyvoniukkate@gmail.com>
2021-03-16 15:21:17 +0200

    Add image ruquester

--- Nursery/app/src/main/java/com/example/nursery/MainActivity.java
+++ Nursery/app/src/main/java/com/example/nursery/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.android.volley.toolbox.NetworkImageView;
+import com.example.nursery.network.ImageRequester;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageRequester imageRequester;
+    private NetworkImageView myImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String url = "https://i2.rozetka.ua/goods/1774405/ferrero_5413548040875_images_1774405319.jpg";
+
+        imageRequester = ImageRequester.getInstance();
+        myImage = findViewById(R.id.myImage);
+        imageRequester.setImageFromUrl(myImage, url);
     }
 }
