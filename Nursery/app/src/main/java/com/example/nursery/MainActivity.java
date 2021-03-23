865cbb927cf2987b86fc36f105c4937d2fbcc658
novakvova <novakvova@gmail.com>
2021-03-23 15:23:16 +0200

    Add my project cat

--- Nursery/app/src/main/java/com/example/nursery/MainActivity.java
+++ Nursery/app/src/main/java/com/example/nursery/MainActivity.java
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String url = "http://10.0.2.2:5000/images/1.jpg";
-
+        //String url = "http://10.0.2.2:5000/images/1.jpg";
+        String url = "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*";
         imageRequester = ImageRequester.getInstance();
         myImage = findViewById(R.id.myImage);
         imageRequester.setImageFromUrl(myImage, url);
