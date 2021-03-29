051b3c2b288761fb0af41512cc8589d646fcfd55
novakvova <novakvova@gmail.com>
2021-03-29 15:44:18 +0300

    Add base projects

---
+++ Nursery.WebApi/Nursery.Domain/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace Nursery.Domain
+{
+    public class AppUser : IdentityUser<long>
+    {
+        public string DisplayName { get; set; }
+        public virtual ICollection<AppUserRole> UserRoles { get; set; }
+    }
+}
